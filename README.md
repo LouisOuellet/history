4615231cc9703382e1534229ea5412ba10698da4
louis <louis_ouellet@hotmail.com>
2020-08-17 09:57:59 -0400

    UPDATE

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# history
-PHP MySQL history
+# PHP MySQL History class
+
+The History class is an extension to the Database class. Little heads up though. This class will increase the amount of queries to your MySQL database. In exchange, you will have a complete log of all the transactions done into your database.
+
+## Change Log
+ * [2020-08-16] - Uploaded to GitHub
+
+## Requirements for the Database Class
+ * PHP
+ * MySQL
+
+### SQL Table
+ * id int(11) AUTO_INCREMENT PRIMARY
+ * created datetime
+ * modified datetime
+ * action varchar(255)
+ * table varchar(255)
+ * before TEXT
+ * after TEXT
+ * ip varchar(255)
+
+## Testing environment
+### Hardware
+ * Dual-Core Intel® Core™ i5-4310U CPU @ 2.00GHz
+ * Intel Corporation Haswell-ULT Integrated Graphics Controller (rev 0b)
+ * 7.9 GB memory
+ * 471.5 GB storage (SATA SSD)
+### Software
+ * elementary OS 5.1.7 Hera
+ * Apache/2.4.39 (Unix)
+ * PHP 7.3.5 (cli) (built: May  3 2019 11:55:32) ( NTS )
+ * MySQL Ver 15.1 Distrib 10.1.39-MariaDB
+
+## Usage
+### Basics
+```php
+require_once('history.php');
+$db = new History('host','username','password','database');
+```
+
+### Example
+```php
+require_once('history.php');
+$db = new History('host','username','password','database');
+
+
+```
---
+++ history.php
@@ -0,0 +1,64 @@
+<?php
+class History extends Database{
+
+  public function __construct($host,$username,$password,$database){
+    parent::__construct($host,$username,$password,$database);
+  }
+
+	private function get_client_ip() {
+	  $ipaddress = '';
+	  if (getenv('HTTP_CLIENT_IP'))
+	    $ipaddress = getenv('HTTP_CLIENT_IP');
+	  else if(getenv('HTTP_X_FORWARDED_FOR'))
+	    $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+	  else if(getenv('HTTP_X_FORWARDED'))
+	    $ipaddress = getenv('HTTP_X_FORWARDED');
+	  else if(getenv('HTTP_FORWARDED_FOR'))
+	    $ipaddress = getenv('HTTP_FORWARDED_FOR');
+	  else if(getenv('HTTP_FORWARDED'))
+	    $ipaddress = getenv('HTTP_FORWARDED');
+	  else if(getenv('REMOTE_ADDR'))
+	    $ipaddress = getenv('REMOTE_ADDR');
+	  else
+	    $ipaddress = 'UNKNOWN';
+	  return $ipaddress;
+	}
+
+	private function saveTransaction($table, $action, $before, $after){
+		$query = [
+			'before' => json_encode($before),
+			'after' => json_encode($after),
+			'action' => $action,
+			'table' => $table,
+			'ip' => $this->get_client_ip(),
+		];
+		parent::create('history',$query);
+	}
+
+	public create($table,$fields){
+		$results = parent::create($table,$field);
+		$this->saveTransaction($table, 'create', [], $fields);
+		return $results;
+	}
+
+	public read($table, $id = null, $field = 'id'){
+		$results = parent::read($table,$id,$field);
+		if($results->numRows() == 1){ $before = $results->fetchArray(); } else { $before = $results->fetchAll(); }
+		$this->saveTransaction($table, 'read', $before, []);
+		return $results;
+	}
+
+	public update($table, $fields, $id, $field = 'id'){
+		$before = parent::read($table,$id,$field)->fetchArray();
+		$results = parent::update($table,$field,$id,$field);
+		$this->saveTransaction($table, 'save', $before, $fields);
+		return $results;
+	}
+
+	public delete($table,$id,$field = 'id'){
+		$before = parent::read($table,$id,$field)->fetchArray();
+		$results = parent::delete($table,$id,$field);
+		$this->saveTransaction($table, 'delete', $before, []);
+		return $results;
+	}
+}
